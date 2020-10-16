1b5546591c729c3a7fe50947924cc2c2546f8be3
Hugo Escandon <hescandon6@gmail.com>
2020-10-16 10:10:30 -0400

    node-fetch

--- tristate-api/eventBrightFetch.js
+++ tristate-api/eventBrightFetch.js
@@ -1,1 +1,16 @@
 const fetch = require("node-fetch");
+const {} = require("./config");
+
+
+
+async function eventFetch() {
+const response = await fetch("")
+
+
+
+
+}.then(async function (response) {
+const data = await response.json()
+
+
+})
