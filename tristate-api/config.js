1e9b8de2b370f36eb87ee54fcf8dea4b4d2bd0e5
Hugo Escandon <hescandon6@gmail.com>
2020-10-16 09:33:16 -0400

    added config file

--- tristate-api/config.js
+++ tristate-api/config.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-
-
+apiKey:  process.env.API_key,
+clientSecret: process.env.Client_secret,
+privateToken: process.env.Private_token,
+publicToken: process.env.Public_token
 };
