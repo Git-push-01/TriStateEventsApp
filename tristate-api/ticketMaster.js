3a180e71d0c3f9f72779498f1e68f1206f9891b0
Hugo Escandon <hescandon6@gmail.com>
2020-10-19 15:49:22 -0400

    update ticket 2.0

--- tristate-api/ticketMaster.js
+++ tristate-api/ticketMaster.js
@@ -6,16 +6,14 @@ async function eventFetch() {
     `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&classificationName=comedy&dmaId=345&page=0&size=10`
   ).then(async function (response) {
     const data = await response.json().then((data) => {
-      const eventData =  data._embedded.events.map((venue) => ({
+      const eventData = data._embedded.events.map((venue) => ({
         eventName: venue.name,
         eventUrl: venue.url,
         eventImg: venue.images[2].url,
         eventNote: venue.pleaseNote,
-
       }));
 
       console.log(eventData);
-
     });
   });
 }
