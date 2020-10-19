a2ffad2d01f70b7887fcc3a7f405407951559268
Hugo Escandon <hescandon6@gmail.com>
2020-10-19 12:20:21 -0400

    event note

--- tristate-api/ticketMaster.js
+++ tristate-api/ticketMaster.js
@@ -3,16 +3,19 @@ const { apiKey } = require("./config");
 
 async function eventFetch() {
   const response = await fetch(
-    `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&marketId=35&page=0&size=100`
+    `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&classificationName=comedy&dmaId=345&page=0&size=10`
   ).then(async function (response) {
     const data = await response.json().then((data) => {
-      const eventData = data._embedded.events.map((venue) => ({
+      const eventData =  data._embedded.events.map((venue) => ({
         eventName: venue.name,
         eventUrl: venue.url,
         eventImg: venue.images[2].url,
+        eventNote: venue.pleaseNote,
+        
       }));
 
       console.log(eventData);
+
     });
   });
 }
