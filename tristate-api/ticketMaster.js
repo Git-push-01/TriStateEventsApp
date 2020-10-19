657c22c00eabda852619ebc1759ba8bffc231640
Hugo Escandon <hescandon6@gmail.com>
2020-10-19 11:04:10 -0400

    eventname, eventurl, eventImg

--- tristate-api/ticketMaster.js
+++ tristate-api/ticketMaster.js
@@ -3,23 +3,17 @@ const { apiKey } = require("./config");
 
 async function eventFetch() {
   const response = await fetch(
-    `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&marketId=35&page=0&size=20`
+    `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&marketId=35&page=0&size=100`
   ).then(async function (response) {
-    const data = await response.json()
-    .then(data => {
-    const newData =   data._embedded.events.map((venue) => ({
+    const data = await response.json().then((data) => {
+      const eventData = data._embedded.events.map((venue) => ({
         eventName: venue.name,
         eventUrl: venue.url,
-        eventImg: venue.images.map((img) => {
-          imgRatio:img.ratio,
-          imgUrl: img.url
+        eventImg: venue.images[2].url,
+      }));
 
-        })
-      }))
-      console.log(newData);
-    })
-
-  })
-
+      console.log(eventData);
+    });
+  });
 }
 eventFetch();
