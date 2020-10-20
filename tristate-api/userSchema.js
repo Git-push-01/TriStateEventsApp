091ecfc0fe519f70259f36387bd7a7ff0bbf5183
Hugo Escandon <hescandon6@gmail.com>
2020-10-20 13:56:33 -0400

    moved stuff around

--- tristate-api/userSchema.js
+++ tristate-api/userSchema.js
@@ -1,6 +1,9 @@
 let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({ email: String, password: String });
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
 const User = mongoose.model("post", UserSchema);
 module.exports = User;
