a6a4222057e9570d9dacba92430e3030e838529e
Hugo Escandon <hescandon6@gmail.com>
2020-10-29 15:22:08 -0400

    get express

--- tristate-api/server.js
+++ tristate-api/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 // const passport = require('passport');
 // const config = require('./db');
+
 
 // mongoose.connect(config.DB, { useNewUrlParser: true }).then(
 //     () => {console.log('Database is connected') },
@@ -11,13 +12,15 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+ // app.use(bodyParser.urlencoded({ extended: false }));
+ // app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-    res.send('hello');
+ res.json()
 });
-
+const eventData = require("./ticketMaster")
+// eventData.then(async function (events) {await  JSON.stringify(events)})
+console.log(eventData);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
--- tristate-front-end/src/App.js
+++ tristate-front-end/src/App.js
@@ -4,16 +4,16 @@ import './App.css';
 
 const  App = () => {
 
-  // const [count, setCount] = useState(0)
-  //
-  //
-  // return (
-  //   <div className="App">
-  //   <button type="button" onClick={() => setCount(count + 1)}>Click me</button>
-  //   <div>Clicks:{count}</div>
-  //
-  //   </div>
-  // );
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <div className="App">
+    <button type="button" onClick={() => setCount(count + 1)}>Click me</button>
+    <div>Clicks:{count}</div>
+
+    </div>
+  );
 }
 
 export default App;
