f6b6902fcb1cb8c33811b878d6f90a3a16414e74
Hugo Escandon <hescandon6@gmail.com>
2020-11-07 16:23:20 -0500

    Added register Route

--- tristate-front-end/src/App.js
+++ tristate-front-end/src/App.js
@@ -3,6 +3,7 @@ import { Card } from "semantic-ui-react";
 import "./App.css";
 import Events from "./events";
 import Navbar from "./components/Navbar";
+import Register from "./components/Register"
 
 const App = () => (
   <Card.Group
@@ -10,6 +11,7 @@ const App = () => (
     rounded="true"
     style={{ minHeight: 100, padding: "1em 0em" }}
   >
+  <Register/>
   <Navbar/>
     <Card>
       <Events />
